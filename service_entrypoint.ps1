7b753aa8d699d8ce4f053b3ab682bb159b9c008a
Sunjay Bhatia <sunjayb@vmware.com>
2020-10-07 02:42:46 +0000

    Modifications for demo
    Signed-off-by: Sunjay Bhatia <sunjayb@vmware.com>

--- service_entrypoint.ps1
+++ service_entrypoint.ps1
@@ -1,4 +1,4 @@
-Start-Process -FilePath "python" -ArgumentList @("./service/main.py") -WorkingDirectory $pwd -RedirectStandardError './serv-err.txt' -RedirectStandardOutput './serv-out.txt'
+Start-Process -FilePath "python" -ArgumentList @("./service/main.py") -WorkingDirectory $pwd -RedirectStandardError './logs/serv-err.log' -RedirectStandardOutput './logs/serv-out.log'
 
 $serviceName = "service$env:ServiceId"
-.\envoy-static.exe --config-path .\envoy-config\envoy-service-config.yaml -l debug --service-cluster $serviceName
+.\envoy-static.exe --config-path .\envoy-config\bootstrap.yaml --log-path .\logs\envoy.log --service-cluster $serviceName --service-node $serviceName-0
